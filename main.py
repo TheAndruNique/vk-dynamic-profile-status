ab8bff1bb77631884034643219d1c065890c9415
TheAndruNique <theandrusyper@gmail.com>
2023-11-15 23:17:26 +0300

    app update

--- main.py
+++ main.py
@@ -4,62 +4,52 @@ import asyncio
 from dotenv import load_dotenv
 import os
 from helpers import get_emoji, get_time_of_day
-from forecast import get_forecast
+from forecast import WeatherManager
 import logging
 
 
 load_dotenv()
 
-user = User(token=os.getenv('TOKEN'))
 
-async def change_status(new_status):
-    await user.api.status.set(new_status)
+class Application:
+    def __init__(self) -> None:
+        self.status_format_str: str = os.getenv('STATUS_FORMAT', '{time} {emoji} | {forecast}')
+        if not os.getenv('STATUS_FORMAT'):
+            logging.warning('Environment variable STATUS_FORMAT not found, using default format')
 
-async def time_in_status():
-    last_status_time = None
-    while True:
-        current_time = datetime.datetime.now()
-        
-        time_of_day = get_time_of_day(current_time)
-        
-        emoji = get_emoji(time_of_day)
-        
-        status_time = current_time.strftime("%I:%M %p")
-        status_text = f'{status_time} {emoji}'
+        self.weather_manager = WeatherManager()
+        self.user = User(token=os.getenv('TOKEN'))
 
-        if last_status_time != status_time:
-            await change_status(status_text)
-            last_status_time = status_time
+    async def change_status(self, new_status):
+        await self.user.api.status.set(new_status)
+    
+    def run(self):
+        asyncio.run(self.main())
+    
+    async def main(self):
+        last_time = None
+        while True:
+            current_time = datetime.datetime.now()
+            forecast = self.weather_manager.get_forecast(current_time.hour)
+            time_of_day = get_time_of_day(current_time)
+            emoji = get_emoji(time_of_day)
+            status_time = current_time.strftime("%I:%M %p")
+            
+            status_formated_string = self.status_format_str.format(
+                forecast = forecast,
+                emoji = emoji,
+                time = status_time
+            )
+            
+            if last_time != status_time:
+                await self.change_status(status_formated_string)
+                last_time = status_time
 
-        await asyncio.sleep(5)
+            await asyncio.sleep(5)
+            
 
-async def time_and_forecast_in_status():
-    last_status_time = None
-    while True:
-        current_time = datetime.datetime.now()
-        forecast = get_forecast(current_time.hour)
-        time_of_day = get_time_of_day(current_time)
-        
-        emoji = get_emoji(time_of_day)
-        
-        status_time = current_time.strftime("%I:%M %p")
-
-        if forecast:
-            status_text = f'{status_time} {emoji} | {forecast}'
-        else:
-            status_text = f'{status_time} {emoji}'
-
-        if last_status_time != status_time:
-            await change_status(status_text)
-            last_status_time = status_time
-
-        await asyncio.sleep(5)
 
 if __name__ == '__main__':
-    FORECAST = os.getenv('FORECAST')
-    if FORECAST == 'True':
-        logging.info('Time and forecast in your profile status.')
-        asyncio.run(time_and_forecast_in_status())
-    else:
-        logging.info('Time in your profile status.')
-        asyncio.run(time_in_status())
+    app = Application()
+    app.run()
+    
